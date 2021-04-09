5eea8de8e1d8bf5123751f2a57490d424e046d51
ChrisLin <i100308326@gmail.com>
2021-04-09 23:45:26 +0800

    no.202

---
+++ 202.happy-number.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=202 lang=javascript
+ *
+ * [202] Happy Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+  let set = new Set()
+  let res = null
+  
+  while (true) {
+      let size = set.size
+      nums = n.toString().split('')
+      n = nums.reduce((acc, curr) => acc + curr ** 2, 0)
+      set.add(n)
+      
+      if (n === 1 || size === set.size) break
+  }
+  
+  return n === 1
+};
+// @lc code=end
+
