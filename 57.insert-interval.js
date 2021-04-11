586f5086985a39f90e45cb1932f5cac5e635fd37
ChrisLin <i100308326@gmail.com>
2021-04-11 12:19:32 +0800

    no.57

--- 57.insert-interval.js
+++ 57.insert-interval.js
@@ -11,7 +11,24 @@
  * @return {number[][]}
  */
 var insert = function(intervals, newInterval) {
-    
+  // 思路 插入之後再合併
+  intervals.push(newInterval)
+  intervals.sort((a, b) => a[0] - b[0] )
+  let firstInterval = intervals.shift()
+  let result = [firstInterval]
+  
+  intervals.forEach((interval) => {
+    // 區間大於 start && 小於end 合併區間  
+    if (result[result.length - 1][1] >= interval[0] && result[result.length - 1][1] < interval[1]) {
+      result[result.length - 1][1] = interval[1]
+    }
+    // 區間小於 start 增加下個區間
+    else if (result[result.length - 1][1] < interval[0]) {
+      result.push(interval)
+    }
+  })
+
+  return result
 };
 // @lc code=end
 
