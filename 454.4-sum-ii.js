756d5787a4577a135068cdc6e74f4fb815021a00
ChrisLin <i100308326@gmail.com>
2021-04-30 13:09:26 +0800

    no.454

---
+++ 454.4-sum-ii.js
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=454 lang=javascript
+ *
+ * [454] 4Sum II
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function(nums1, nums2, nums3, nums4) {
+  const map = new Map()
+  let count = 0
+  
+  for (const num1 of nums1) {
+    for (const num2 of nums2) {
+      const sum = num1 + num2
+      map.set(sum, (map.get(sum) || 0) + 1)
+    }
+  }
+  
+  for (const num3 of nums3) {
+    for (const num4 of nums4) {
+      count += map.get(- num3 - num4) || 0
+    }
+  }
+  
+  return count
+};
+// @lc code=end
+
