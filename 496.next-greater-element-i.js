caaabdb8cdc2e79ce711c60e16d24fbedb727fc1
ChrisLin <i100308326@gmail.com>
2021-05-04 23:35:12 +0800

    no.496

--- 496.next-greater-element-i.js
+++ 496.next-greater-element-i.js
@@ -12,36 +12,26 @@
  */
 var nextGreaterElement = function(nums1, nums2) {
   const result = Array(nums2.length).fill(-1)
-  const monoStack = []
-  
-  for (let i = 0; i < nums2.length; ++i) {
-      for (let j = monoStack.length - 1;j >= 0; --j) {
-          if (nums2[i] > nums2[monoStack[j]] && result[monoStack[j]] === -1) {
-              result[monoStack[j]] = nums2[i]
-              monoStack.pop()
-          } else {
-              break
-          }
-      }
-      monoStack.push(i)
-  }
-  
-  const indexList = []
-  for (let num of nums1) {
-      for (let i = 0;i < nums2.length; ++i) {
-          if (nums2[i] === num) {
-              indexList.push(i)
-              break
-          }
-      }
-  }
-  
-  let answer = []
-  for (let index of indexList) {
-      answer.push(result[index])
-  }
-  
-  return answer
+    const monoStack = []
+    
+    for (let i = 0; i < nums2.length; ++i) {
+        for (let j = monoStack.length - 1;j >= 0; --j) {
+            if (nums2[i] > nums2[monoStack[j]] && result[monoStack[j]] === -1) {
+                result[monoStack[j]] = nums2[i]
+                monoStack.pop()
+            } else {
+                break
+            }
+        }
+        monoStack.push(i)
+    }
+    
+    const map = {}
+    for (let i =0 ; i < nums2.length; ++i) map[nums2[i]] = i
+    
+    let answer = []
+    for (let i = 0; i < nums1.length; ++i ) answer.push(result[map[nums1[i]]])
+    return answer
 };
 // @lc code=end
 
