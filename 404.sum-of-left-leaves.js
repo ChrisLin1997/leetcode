6387833738ff60a399d88df53fa609a1ce009099
ChrisLin <i100308326@gmail.com>
2021-05-13 23:54:17 +0800

    no.404

---
+++ 404.sum-of-left-leaves.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=404 lang=javascript
+ *
+ * [404] Sum of Left Leaves
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+  if (root === null) return 0
+  if (isLeft && root.left === null && root.right === null) return root.val
+  return sumOfLeftLeaves(root.left, true) + sumOfLeftLeaves(root.right)
+};
+// @lc code=end
+
