5bbf98dd280744387c6195f4548ea0c891aaf86d
ChrisLin <i100308326@gmail.com>
2021-05-27 22:06:49 +0800

    no.455

---
+++ 455.assign-cookies.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode id=455 lang=javascript
+ *
+ * [455] Assign Cookies
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} g
+ * @param {number[]} s
+ * @return {number}
+ */
+var findContentChildren = function(g, s) {
+  g.sort((a, b) => a - b)
+  s.sort((a, b) => a - b)
+  
+  let result = 0
+  let index = s.length - 1
+  for (let i = g.length - 1; i >= 0; --i ) {
+      if (index >= 0 && s[index] >= g[i]) {
+          ++result
+          --index
+      }
+  }
+  
+  return result
+};
+// @lc code=end
+
