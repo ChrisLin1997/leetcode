748c3f292ab695fdbd50634e2849029594778b7e
ChrisLin <i100308326@gmail.com>
2021-05-27 22:05:11 +0800

    no.53

---
+++ 53.maximum-subarray.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=53 lang=javascript
+ *
+ * [53] Maximum Subarray
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+  let result = -Infinity
+  let sum = 0
+  for (let i = 0; i < nums.length; ++i) {
+      sum += nums[i]
+      if (sum > result) result = sum
+      if (sum < 0) sum = 0
+  }
+  return result
+};
+// @lc code=end
+
