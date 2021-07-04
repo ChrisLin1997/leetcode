2938ee85788fdb51c8c1bb925d845121619847d9
ChrisLin <i100308326@gmail.com>
2021-07-04 21:56:51 +0800

    no.1049

--- 1049.last-stone-weight-ii.js
+++ 1049.last-stone-weight-ii.js
@@ -10,20 +10,17 @@
  * @return {number}
  */
 var lastStoneWeightII = function(stones) {
-  let sum = 0
-  for (let num of stones) sum += num
-  let half = Math.floor(sum / 2)
-  
-  const dp = Array(stones.length + 1).fill().map(() => Array(half + 1).fill(0))
-  
-  for (let i = 1; i <= stones.length; ++i) {
-      for (let j = 0; j <= half; ++j) {
-          if (stones[i - 1] <= j) dp[i][j] = Math.max(stones[i -1] + dp[i - 1][j - stones[i - 1]], dp[i - 1][j])
-          else dp[i][j] = dp[i - 1][j]
+  const sum = stones.reduce((acc, curr) => acc + curr)
+  const half = Math.floor(sum / 2)
+  const dp = Array(half + 1).fill(0)
+        
+  for (let i = 0; i < stones.length; ++i) {
+      for (let j = half; j >= 0; --j) {
+          if (j >= stones[i]) dp[j] = Math.max(stones[i] + dp[j - stones[i]], dp[j])
       }
   }
   
-  return sum - 2 * dp[stones.length][half]
+  return sum - 2 * dp[half]
 };
 // @lc code=end
 
